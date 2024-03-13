695845e47cbea52d4b91f06a94ef06dbd1a41221
Jérémy Laurent <poulpy2k@gmail.com>
2024-03-13 17:45:14 +0100

    build(*): initial commit 🚀

---
+++ Watowatch/WatowatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatowatchApp.swift
+//  Watowatch
+//
+//  Created by Jérémy Laurent on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatowatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
